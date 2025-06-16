6d6693a092ff9a93b9f34dc99b5f0cf4ba57fab1
saumittras <saumittra.eee@gmail.com>
2025-06-16 17:44:30 +0600

    added .env for sequre DB Connection

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
+import "dotenv/config";
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 
 const PORT = 5000;
 let server: Server;
+const connectUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.s8hxf.mongodb.net/noteDB?retryWrites=true&w=majority&appName=Cluster0`;
 
 async function main() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://mongo_DB:mongoDB14@cluster0.s8hxf.mongodb.net/noteDB?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(connectUri);
     console.log("Mongoose is connected successfully");
     server = app.listen(PORT, () => {
       console.log(`App is listening on post: ${PORT}`);
