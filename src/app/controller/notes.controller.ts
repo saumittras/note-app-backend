f315b3fd0f6d674d6e0cf2a1f4a0a2311f9c37b1
saumittras <saumittra.eee@gmail.com>
2025-06-16 23:08:21 +0600

    added MVC architecture

---
+++ src/app/controller/notes.controller.ts
@@ -0,0 +1,69 @@
+import express, { Request, Response } from "express";
+import { ObjectId } from "mongodb";
+import { Note } from "../model/notes.model";
+
+export const notesRoutes = express.Router();
+
+notesRoutes.post("/create-note", async (req: Request, res: Response) => {
+  const body = req.body;
+  console.log(body);
+
+  const note = await Note.create(body);
+
+  res.status(201).json({
+    success: true,
+    message: "Note created successfully",
+    note: note,
+    body: body,
+  });
+});
+
+notesRoutes.get("/", async (req: Request, res: Response) => {
+  const note = await Note.find();
+
+  res.status(201).json({
+    success: true,
+    message: "Get all Note Data successfully",
+    note: note,
+  });
+});
+
+notesRoutes.get("/:noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const note = await Note.findById(noteId);
+  let message;
+  if (note === null) {
+    message = "Note data not found";
+  } else {
+    message = "Note Data get successfully";
+  }
+  res.status(201).json({
+    success: true,
+    message: message,
+    note: note,
+  });
+});
+
+notesRoutes.patch("/:noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const updatedBody = req.body;
+
+  const note = await Note.findByIdAndUpdate(noteId, updatedBody, { new: true });
+
+  res.status(201).json({
+    success: true,
+    message: "Note Updated successfully",
+    note,
+  });
+});
+
+notesRoutes.delete("/:noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const note = await Note.deleteOne({ _id: new ObjectId(noteId) });
+
+  res.status(201).json({
+    success: true,
+    message: "Note data Deleted successfully",
+    note: note,
+  });
+});
---
+++ src/app/model/notes.model.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from "mongoose";
+
+const noteSchema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    content: { type: String, defaultg: "" },
+    category: {
+      type: String,
+      enum: ["personal", "work", "study", "other"],
+      default: "personal",
+    },
+    pinned: {
+      type: Boolean,
+      default: false,
+    },
+    tags: {
+      label: { type: String, required: true },
+      color: { type: String, default: "gray" },
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const Note = model("Note", noteSchema);
